7be4dc171a6aa55fc179fdd19cb90aafdf1194a5
Alberto-Ignacio-Flores-Mendoza <56567492+Alberto-Ignacio-Flores-Mendoza@users.noreply.github.com>
2023-09-03 22:48:30 -0600

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <secrion className="section about-section">
+      <h1 className='section-title'> About us</h1>
+      <p> asdf asff asfasfa sfafasf asfasf asfasf asfasf asfasfa
+        afasf asfasf asfasfa asfafas asffasf asfasf asfsafas sfafsa fsafsa
+        asfasfas fasfasf safffas sfasfa fasfasf safsaf saf afsaf asf asfsa sfafasfsafas
+        aboutafa
+        afasfffs faaf as
+        afasfssfasf asfasfasfasfasfsafasfasfasfa sfasf afa asf fas
+        asfasf asfsafasfa asfasfas fafasfasfas fasfassfafsafs afaasfasf
+        asfasf asfasfas fsaf as
+        asfasfasf asffas
+        afafasa safasf asfasfa asfasfasf asfas afa fasf asfafasaf
+      </p>
+    </secrion>
+  )
+}
+
+export default About
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import CocktailList from '../Components/CocktailList'
+import SearchForm from '../Components/SearchForm'
+
+const Home = () => {
+  return (
+   
+    <main>
+      <SearchForm>
+      </SearchForm>
+      <CocktailList>
+      </CocktailList>
+      
+    </main>
+
+  )
+}
+
+export default Home
